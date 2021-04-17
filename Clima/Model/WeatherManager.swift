9cd759e91564db1fb385f1abe03cc14acb0560d9
karimovk <karina.karimova@ut.ee>
2021-04-17 17:39:34 +0300

    URLSession

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,10 +9,37 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=42e0890b6de97734838cb5801bb12141&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=42e0890b6de97734838cb5801bb12141&units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        // 1. Create URL
+        if let url = URL(string: urlString) {
+            // 2. Create URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session the task
+            let task = session.dataTask(with: url, completionHandler: handler(data:response:error:))
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handler (data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+    
 }
